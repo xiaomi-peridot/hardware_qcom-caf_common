e2ca7293a0bb766e764554541286066b93e9e976
HeroBuxx <me@herobuxx.me>
2024-07-15 15:24:19 +0700

    common: Include legacy boards in UM 5.15 family conditionally
    Apparently, bengal has two kernel versions, 4.19 and 5.15. So, let's handle them properly.
    
    This change conditionally adds bengal to UM_5_15_FAMILY and removes it from UM_4_19_FAMILY if TARGET_BOARD_SUFFIX is set to _515 and TARGET_BOARD_PLATFORM is bengal.
    
    This also applies to kona and lito, but so far, Qualcomm hasn't released any source code, and no kona devices have been launched with kernel version 5.15 yet.
    If there's any we could add them into UM_5_15_FAMILY_LEGACY later on.
    
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- qcom_defs.mk
+++ qcom_defs.mk
@@ -2,12 +2,28 @@
 MSMSTEPPE := sm6150
 TRINKET := trinket #SM6125
 
+UM_5_15_FAMILY_LEGACY := bengal
+
 # UM families
 UM_3_18_FAMILY := msm8996 msm8937 msm8953
 UM_4_4_FAMILY := msm8998 sdm660
 UM_4_9_FAMILY := sdm845 sdm710
 UM_4_14_FAMILY := msmnile $(MSMSTEPPE) $(TRINKET) atoll
-UM_4_19_FAMILY := kona lito bengal
+UM_4_19_FAMILY_BASE := kona lito
 UM_5_4_FAMILY := lahaina holi
 UM_5_10_FAMILY := taro parrot
-UM_5_15_FAMILY := kalama crow
+UM_5_15_FAMILY_BASE := kalama crow
+
+# Conditionally modify the UM families based on TARGET_BOARD_SUFFIX and TARGET_BOARD_PLATFORM
+ifneq ($(filter $(UM_5_15_FAMILY_LEGACY),$(TARGET_BOARD_PLATFORM)),)
+    ifeq ($(TARGET_BOARD_SUFFIX),_515)
+        UM_4_19_FAMILY := $(UM_4_19_FAMILY_BASE)
+        UM_5_15_FAMILY := $(UM_5_15_FAMILY_BASE) $(UM_5_15_FAMILY_LEGACY)
+    else
+        UM_4_19_FAMILY := $(UM_4_19_FAMILY_BASE) bengal
+        UM_5_15_FAMILY := $(UM_5_15_FAMILY_BASE)
+    endif
+else
+    UM_4_19_FAMILY := $(UM_4_19_FAMILY_BASE) bengal
+    UM_5_15_FAMILY := $(UM_5_15_FAMILY_BASE)
+endif
